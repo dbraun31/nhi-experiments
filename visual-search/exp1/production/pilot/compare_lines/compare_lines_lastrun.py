65651c11ab0023240924fa38f1d4c5e3364666c6
Dave Braun <dab414@lehigh.edu>
2021-07-09 12:25:40 -0400

    Add excel conditions and timing

--- visual-search/exp1/production/pilot/compare_lines/compare_lines_lastrun.py
+++ visual-search/exp1/production/pilot/compare_lines/compare_lines_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.4),
-    on Thu 08 Jul 2021 12:14:26 PM EDT
+    on Fri 09 Jul 2021 12:01:42 PM EDT
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -29,12 +29,16 @@ import sys  # to get file system encoding
 from psychopy.hardware import keyboard
 
 import psychopy
+import time\
 
-def determine_diagonal(start_coord_left_line, length, direction = 1):
+def determine_diagonal(start_coord_left_line, length, x_direction, y_direction):
+    x_shift = 1 if x_direction == 'right' else -1
+    y_shift = 1 if y_direction == 'up' else -1
+    
     start_x = start_coord_left_line[0]
     start_y = start_coord_left_line[1]
-    end_coord_left_line_x = start_x + direction * (length * cos(1.0472))
-    end_coord_left_line_y = start_y - (length * sin(1.0472))
+    end_coord_left_line_x = start_x + x_shift* (length * cos(1.0472))
+    end_coord_left_line_y = start_y + y_shift* (length * sin(1.0472))
     
     return (end_coord_left_line_x, end_coord_left_line_y)
 
@@ -59,8 +63,8 @@ yy = y + (d * sin(alpha))
 '''
 
 length = 300
-starting_point_left_x = -100
-distance_between_lines = 200
+starting_point_left_x = -300
+distance_between_lines = 600
 ## horizontal lines always zero
 starting_point_y = length / 2
 
@@ -78,6 +82,21 @@ horizontal_ending_point_line1 = (starting_point_left_x + (length / 2), 0)
 horizontal_starting_point_line2 = (horizontal_starting_point_line1[0] + distance_between_lines, 0)
 horizontal_ending_point_line2  = (horizontal_ending_point_line1[0] + distance_between_lines, 0)
 
+## DIAGONAL LINES
+## forward slash (ie starts at top and goes forward)
+forward_starting_point_line1 = determine_diagonal((starting_point_left_x, 0), length/2, x_direction = 'left', y_direction = 'up')
+forward_ending_point_line1 = determine_diagonal((starting_point_left_x, 0), length/2, x_direction = 'right', y_direction = 'down')
+forward_starting_point_line2 = (forward_starting_point_line1[0] + distance_between_lines, forward_starting_point_line1[1])
+forward_ending_point_line2 = (forward_ending_point_line1[0] + distance_between_lines, forward_ending_point_line1[1])
+
+## backward slash (ie starts at top and goes back)
+backward_starting_point_line1 = determine_diagonal((starting_point_left_x, 0), length/2, x_direction = 'right', y_direction = 'up')
+backward_ending_point_line1 = determine_diagonal((starting_point_left_x, 0), length/2, x_direction = 'left', y_direction = 'down')
+backward_starting_point_line2 = (backward_starting_point_line1[0] + distance_between_lines, backward_starting_point_line1[1])
+backward_ending_point_line2 = (backward_ending_point_line1[0] + distance_between_lines, backward_ending_point_line1[1])
+
+
+
 RotationCondition = {
 'vertical': [[vertical_starting_point_line1, vertical_ending_point_line1], 
 [vertical_starting_point_line2, vertical_ending_point_line2]],
@@ -85,8 +104,8 @@ RotationCondition = {
 'horizontal': [[horizontal_starting_point_line1, horizontal_ending_point_line1], 
 [horizontal_starting_point_line2, horizontal_ending_point_line2]],
 
-'diagonal1': [[(-200, 50), determine_diagonal((-200, 50), length)], [(200, 50), determine_diagonal((200, 50), length)]],
-'diagonal2': [[(-200, 50), determine_diagonal((-200, 50), length, direction = -1)], [(200, 50), determine_diagonal((200, 50), length, direction = -1)]]
+'diagonal1': [[forward_starting_point_line1, forward_ending_point_line1], [forward_starting_point_line2, forward_ending_point_line2]],
+'diagonal2': [[backward_starting_point_line1, backward_ending_point_line1], [backward_starting_point_line2, backward_ending_point_line2]]
 }
 
 
@@ -145,6 +164,26 @@ else:
 # create a default keyboard (e.g. to check for escape)
 defaultKeyboard = keyboard.Keyboard()
 
+# Initialize components for Routine "ISI"
+ISIClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text=None,
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+
+# Initialize components for Routine "soa_slide"
+soa_slideClock = core.Clock()
+text_2 = visual.TextStim(win=win, name='text_2',
+    text=None,
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=-2.0);
+
 # Initialize components for Routine "trial"
 trialClock = core.Clock()
 key_resp = keyboard.Keyboard()
@@ -154,9 +193,9 @@ globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
 # set up handler to look after randomisation of conditions etc
-trials = data.TrialHandler(nReps=5.0, method='random', 
+trials = data.TrialHandler(nReps=10.0, method='random', 
     extraInfo=expInfo, originPath=-1,
-    trialList=[None],
+    trialList=data.importConditions('conditions.csv'),
     seed=None, name='trials')
 thisExp.addLoop(trials)  # add the loop to the experiment
 thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
@@ -172,12 +211,173 @@ for thisTrial in trials:
         for paramName in thisTrial:
             exec('{} = thisTrial[paramName]'.format(paramName))
     
+    # ------Prepare to start Routine "ISI"-------
+    continueRoutine = True
+    routineTimer.add(0.500000)
+    # update component parameters for each repeat
+    # keep track of which components have finished
+    ISIComponents = [text]
+    for thisComponent in ISIComponents:
+        thisComponent.tStart = None
+        thisComponent.tStop = None
+        thisComponent.tStartRefresh = None
+        thisComponent.tStopRefresh = None
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    # reset timers
+    t = 0
+    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+    ISIClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+    frameN = -1
+    
+    # -------Run Routine "ISI"-------
+    while continueRoutine and routineTimer.getTime() > 0:
+        # get current time
+        t = ISIClock.getTime()
+        tThisFlip = win.getFutureFlipTime(clock=ISIClock)
+        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *text* updates
+        if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+            # keep track of start time/frame for later
+            text.frameNStart = frameN  # exact frame index
+            text.tStart = t  # local t and not account for scr refresh
+            text.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+            text.setAutoDraw(True)
+        if text.status == STARTED:
+            # is it time to stop? (based on global clock, using actual start)
+            if tThisFlipGlobal > text.tStartRefresh + 0.5-frameTolerance:
+                # keep track of stop time/frame for later
+                text.tStop = t  # not accounting for scr refresh
+                text.frameNStop = frameN  # exact frame index
+                win.timeOnFlip(text, 'tStopRefresh')  # time at next scr refresh
+                text.setAutoDraw(False)
+        
+        # check for quit (typically the Esc key)
+        if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in ISIComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "ISI"-------
+    for thisComponent in ISIComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    trials.addData('text.started', text.tStartRefresh)
+    trials.addData('text.stopped', text.tStopRefresh)
+    
+    # ------Prepare to start Routine "soa_slide"-------
+    continueRoutine = True
+    # update component parameters for each repeat
+    ## line_orientation variable comes from excel sheet
+    start_left_line = RotationCondition[line_orientation][0][0]
+    end_left_line = RotationCondition[line_orientation][0][1]
+    start_right_line = RotationCondition[line_orientation][1][0]
+    end_right_line = RotationCondition[line_orientation][1][1]
+    
+    if np.random.uniform() > .5:
+        chosen_line = (start_left_line, end_left_line)
+    else:
+        chosen_line = (start_right_line, end_right_line)
+    
+    
+    text_2.setText('')
+    # keep track of which components have finished
+    soa_slideComponents = [text_2]
+    for thisComponent in soa_slideComponents:
+        thisComponent.tStart = None
+        thisComponent.tStop = None
+        thisComponent.tStartRefresh = None
+        thisComponent.tStopRefresh = None
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    # reset timers
+    t = 0
+    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+    soa_slideClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+    frameN = -1
+    
+    # -------Run Routine "soa_slide"-------
+    while continueRoutine:
+        # get current time
+        t = soa_slideClock.getTime()
+        tThisFlip = win.getFutureFlipTime(clock=soa_slideClock)
+        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        draw_line(chosen_line[0], chosen_line[1])
+        
+        
+        # *text_2* updates
+        if text_2.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+            # keep track of start time/frame for later
+            text_2.frameNStart = frameN  # exact frame index
+            text_2.tStart = t  # local t and not account for scr refresh
+            text_2.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(text_2, 'tStartRefresh')  # time at next scr refresh
+            text_2.setAutoDraw(True)
+        if text_2.status == STARTED:
+            # is it time to stop? (based on global clock, using actual start)
+            if tThisFlipGlobal > text_2.tStartRefresh + stimulus_offset-frameTolerance:
+                # keep track of stop time/frame for later
+                text_2.tStop = t  # not accounting for scr refresh
+                text_2.frameNStop = frameN  # exact frame index
+                win.timeOnFlip(text_2, 'tStopRefresh')  # time at next scr refresh
+                text_2.setAutoDraw(False)
+        
+        # check for quit (typically the Esc key)
+        if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in soa_slideComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "soa_slide"-------
+    for thisComponent in soa_slideComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    trials.addData('text_2.started', text_2.tStartRefresh)
+    trials.addData('text_2.stopped', text_2.tStopRefresh)
+    # the Routine "soa_slide" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
+    
     # ------Prepare to start Routine "trial"-------
     continueRoutine = True
     # update component parameters for each repeat
     key_resp.keys = []
     key_resp.rt = []
     _key_resp_allKeys = []
+    ## line_orientation variable comes from excel sheet
+    start_left_line = RotationCondition[line_orientation][0][0]
+    end_left_line = RotationCondition[line_orientation][0][1]
+    start_right_line = RotationCondition[line_orientation][1][0]
+    end_right_line = RotationCondition[line_orientation][1][1]
+    
     # keep track of which components have finished
     trialComponents = [key_resp]
     for thisComponent in trialComponents:
@@ -223,17 +423,10 @@ for thisTrial in trials:
                 key_resp.rt = _key_resp_allKeys[-1].rt
                 # a response ends the routine
                 continueRoutine = False
-        condition = 'horizontal'
-        
-        start_left_line = RotationCondition[condition][0][0]
-        end_left_line = RotationCondition[condition][0][1]
-        start_right_line = RotationCondition[condition][1][0]
-        end_right_line = RotationCondition[condition][1][1]
-        
-        
-        
         draw_line(start_left_line, end_left_line)
         draw_line(start_right_line, end_right_line)
+        
+        
         
         # check for quit (typically the Esc key)
         if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
@@ -268,7 +461,7 @@ for thisTrial in trials:
     routineTimer.reset()
     thisExp.nextEntry()
     
-# completed 5.0 repeats of 'trials'
+# completed 10.0 repeats of 'trials'
 
 
 # Flip one final time so any remaining win.callOnFlip() 
