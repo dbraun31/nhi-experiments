43371f4cba41d73c5993b25595bcfed616d83cfb
Lily Spencer <lilyspencer@Lilys-MacBook-Pro.local>
2021-07-14 16:03:04 -0400

    Lily

--- visual-search/exp1/production/pilot/compare_lines/compare_lines_lastrun.py
+++ visual-search/exp1/production/pilot/compare_lines/compare_lines_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.4),
-    on Tue 13 Jul 2021 01:43:45 PM EDT
+    on Wed Jul 14 15:58:35 2021
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -51,7 +51,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='compare_lines_lastrun.py',
+    originPath='/Users/lilyspencer/NHI/nhi-experiments/visual-search/exp1/production/pilot/compare_lines/compare_lines_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
