cedbce03f975db9e0db6fed2447ab867d441724a
Dave Braun <dab414@lehigh.edu>
2021-09-10 11:23:04 -0400

    new commit

--- visual-search/exp1/production/visual-search_exp1_lastrun.py
+++ visual-search/exp1/production/visual-search_exp1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.4),
-    on Fri 10 Sep 2021 09:46:54 AM EDT
+    on Fri 10 Sep 2021 10:38:28 AM EDT
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
